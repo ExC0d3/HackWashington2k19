00b29dc3fc14faf6f52bb0036e3fd1de032f3332
cjmediran <16298278+cjmediran@users.noreply.github.com>
2019-01-12 21:59:32 -0800

    image description in binary format

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,31 +1,43 @@
 var express = require('express');
 const microsofComputerVision = require("microsoft-computer-vision")
 var router = express.Router();
-
+const fs = require('fs');
+const path = require('path');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',function(req, res, next) {
     
     res.send('OK');
 });
 
-router.get('/analyzeImage', function(req, res, next){
+router.post('/analyzeImage', function(req, res, next){
   console.log("console");
+  
   var subscriptionKey = "c89b989b989d48f1a0fc5d2533cfbf03";
 
     var image_url = 'https://wallpaperbrowse.com/media/images/750795.jpg';
-
+    
+    console.log('File:', req.file);
+    try{
+        const data = fs.readFileSync(path.resolve('../','backend','uploads','image.png'));
+    }
+    catch(e){
+        console.log('Error: ',e);
+    }
+    
+    
     microsofComputerVision.analyzeImage({
         "Ocp-Apim-Subscription-Key": subscriptionKey,
         "request-origin":"westus2",
-        "content-type": "application/json",
-        "url": image_url,
+        "content-type": "application/octet-stream",
+        "body":req.file.buffer,
         "visual-features":"Categories,Tags,Description,Faces,ImageType,Color,Adult"
-    }).then((result) => {
-        console.log(result)
-        //res.send(result.description.captions[0].text);
-    }).catch((err)=>{
-        res.send(err);
-        throw err
+    })
+    .then((data) => {
+        console.log(data);
+        res.send(data)
+    })
+    .catch((err) => {
+        throw err;
     })
 });
 
